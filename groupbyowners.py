5d923a1a82cd0e4795c92df810bf1217c672ca0b
jeenarita <153416019+jeenarita@users.noreply.github.com>
2023-12-10 12:09:09 +0530

    Update groupbyowners.py

--- groupbyowners.py
+++ groupbyowners.py
@@ -1,1 +1,14 @@
+def group_by_owners(files):
+    result = {}
+    for key, value in files.items():
+        result[value] = result.get(value, []) + [key]
+    
+    return result
 
+if __name__ == "__main__":    
+    files = {
+        'Input.txt': 'Randy',
+        'Code.py': 'Stan',
+        'Output.txt': 'Randy'
+    }   
+    print(group_by_owners(files))
